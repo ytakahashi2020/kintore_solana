a240c0f5860ce9512ce99d09eb4d0cd3cb56cc05
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-05-12 14:43:18 +0900

    versionedTransactionの追加

---
+++ 6_versioned_transaction/1-1/client.ts
@@ -0,0 +1,33 @@
+import { Keypair } from "@solana/web3.js";
+
+// connect to the cluster and get the minimum rent for rent exempt status
+const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+let minRent = await connection.getMinimumBalanceForRentExemption(0);
+let blockhash = await connection
+  .getLatestBlockhash()
+  .then((res) => res.blockhash);
+
+const payer = pg.wallet;
+const toAccount = new Keypair();
+// create an array with your desired `instructions`
+const instructions = [
+  web3.SystemProgram.transfer({
+    fromPubkey: payer.publicKey,
+    toPubkey: toAccount.publicKey,
+    lamports: minRent,
+  }),
+];
+
+const messageV0 = new web3.TransactionMessage({
+  payerKey: payer.publicKey,
+  recentBlockhash: blockhash,
+  instructions,
+}).compileToV0Message();
+
+const transaction = new web3.VersionedTransaction(messageV0);
+
+// sign your transaction with the required `Signers`
+transaction.sign([payer.keypair]);
+
+const txId = await connection.sendTransaction(transaction);
+console.log(`https://explorer.solana.com/tx/${txId}?cluster=devnet`);
