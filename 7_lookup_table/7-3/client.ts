7a083436c60c49b3061df6bf292bfe5413e7425c
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-05-21 06:55:47 +0900

    ルックアップテーブルの作成

--- 7_lookup_table/7-3/client.ts
+++ 7_lookup_table/7-3/client.ts
@@ -1,21 +1,19 @@
 const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
 
 // Define the 'PublicKey' of the lookup table to fetch
-const lookupTableAddress = new web3.PublicKey(
-  "oacZ24DG5vsL1dCPxjY8MsqAzzgAB2DbQo8ziJk3Tpw"
+const lookupTableAddress = new web3(
+  /*Publickeyオブジェクトの作成*/ "oacZ24DG5vsL1dCPxjY8MsqAzzgAB2DbQo8ziJk3Tpw"
 );
 
 // Get the table from the cluster
-const lookupTableAccount = (
-  await connection.getAddressLookupTable(lookupTableAddress)
-).value;
+const lookupTableAccount = await; /* ルックアップテーブルの取得 */
 
 // 'lookupTableAccount' will now be an 'AddressLookupTableAccount' object
 
 console.log("Table address from cluster:", lookupTableAccount.key.toBase58());
 
 // Loop through and parse all the addresses stored in the table
-for (let i = 0; i < lookupTableAccount.state.addresses.length; i++) {
-  const address = lookupTableAccount.state.addresses[i];
+for (let i = 0; i < lookupTableAccount /* */; i++) {
+  const address = lookupTableAccount; /*  */
   console.log(i, address.toBase58());
 }
